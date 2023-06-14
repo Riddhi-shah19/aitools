fc05319326a88049e184608d0d0b76cec59b768f
Riddhi-shah19 <shriddhi19@gmail.com>
2023-06-14 11:30:10 +0530

    hi

--- src/components/Ai_tools.jsx
+++ src/components/Ai_tools.jsx
@@ -1,39 +1,23 @@
 import React from 'react'
 
-
 const Ai_tools = ({data}) => {
  
     const listingComponents=data.map((listing)=>(
         <div className='flex flex-col gap-1' key={listing.id}>
-        <img className='h-36 m-auto pt-5' src={listing.image}/>
+        <img className=' m-auto w-36 border-solid border-[1px] border-black rounded-[50%]' src={listing.image}/>
         <a className='hover:scale-105 hover:underline cursor-pointer text-center mt-2 text-xl' href={listing.link} target='_blank'>{listing.title}</a>
         <p className='text-center'>{listing.description}</p>
         </div>
     ))
   return (
-<<<<<<< HEAD
     <>
     <div id='aitools' className='w-full'>
-    <h1 className='text-center text-3xl font-bold mt-6'>AI TOOLS</h1>
+    <h1 className='text-center text-3xl font-bold mt-6 mb-8 '>AI TOOLS</h1>
     <div className='grid grid-cols-5 justify-center mb-6'>
-=======
-    
-
-    
-
-
-
-    <div id='aitools'>
-    <h1 className='text-center text-3xl font-bold mt-6'>AI TOOLS</h1>
-    <div className='grid grid-cols-5 justify-center mb-6'>
-
-   
->>>>>>> 9488423937599518f1e89eddf317cca1ef4a4790
         {listingComponents}
     </div>
-    
     </div>
-    
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,8 @@ import {React} from 'react';
 import Hero from './Hero';
 
 import { handleScroll,handleScroll2 } from './Hero';
-<<<<<<< HEAD
 
 const Header = () => {
-=======
-
-
-
-
-
-
-const Header = () => {
- 
-  
-
->>>>>>> 9488423937599518f1e89eddf317cca1ef4a4790
   return (
     <>
     <div className='fixed top-0 flex justify-between w-full items-center h-20 px-4 z-10 text-white bg-slate-900'>
@@ -35,14 +22,6 @@ const Header = () => {
         </ul>
     </div>
     <Hero />
-<<<<<<< HEAD
-=======
-
-        
-   
-    
-
->>>>>>> 9488423937599518f1e89eddf317cca1ef4a4790
     </>
   )
 }
